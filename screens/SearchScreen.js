de22ef62b396bb1589ac2525e31445bf11b98a52
lawyerh <lawyerh01@gmail.com>
2024-09-05 23:13:19 -0700

    added stack navigation base

---
+++ screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet as s } from "react-native";
+
+export default function SearchScreen() {
+  return (
+    <View style={styles.view}>
+      <Text>Search page</Text>
+    </View>
+  );
+}
+
+const styles = s.create({
+  view: {},
+});
